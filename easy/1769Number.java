949b2b35733752349bd00225aa8f217bea300f5b
Bhargav Nagabhyru <srisaibhargav2005@gmail.com>
2025-01-07 06:58:39 +0530

    Added LeetCode solutions categorized by difficulty

---
+++ easy/1769Number.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] minOperations(String boxes) {
+        int n = boxes.length();
+        int [] ans = new int[n];
+        int cumval =0;
+        int cumsum =0;
+        for(int i=0;i<n;i++)
+        {
+            ans[i] = cumsum;
+             cumval += boxes.charAt(i) == '0' ? 0 : 1;
+            cumsum +=cumval;
+        }
+         cumval =0;
+         cumsum =0;
+         for(int i=n-1;i>=0;i--)
+         {
+            ans[i] +=cumsum;
+            cumval += boxes.charAt(i) == '0' ? 0 : 1;
+            cumsum+=cumval;
+
+         }
+         return ans;
+
+        
+    }
+}
